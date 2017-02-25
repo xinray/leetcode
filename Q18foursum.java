60ae1c949853f19b421e6e6b2744e978a15285b6
ray <277450201@qq.com>
2017-02-25 21:42:23 +0800

    sum

---
+++ Q18foursum.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ray on 1/3/17.
+ */
+public class foursum {
+    public class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> resultList = new ArrayList<List<Integer>>();
+
+            return resultList;
+        }
+    }
+}
