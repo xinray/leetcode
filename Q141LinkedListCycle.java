e1820129c56a777a8df97f363ec616da7ca636fe
ray <277450201@qq.com>
2017-03-26 22:08:38 +0800

    Q141finish

---
+++ Q141LinkedListCycle.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * Created by ray on 3/26/17.
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class linkedlistcycle {
+    public boolean hasCycle(ListNode head) {
+
+        if(head==null) return false;
+
+        ListNode first = head;
+        ListNode second = head;
+
+        while (second.next != null  && second.next.next != null ) {
+            first = first.next;
+            second = second.next.next;
+
+            if(first == second) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
