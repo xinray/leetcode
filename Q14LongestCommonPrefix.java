925f58a7c0f7f49fe203c7f08c2a4ca4ab8637b6
ray <277450201@qq.com>
2017-03-27 22:38:31 +0800

    Q13 14 finish

---
+++ Q14LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * Created by ray on 3/27/17.
+ */
+public class Q5LongestPalindromicSubstring {
+
+
+    public class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            if(strs == null || strs.length == 0)    return "";
+            String pre = strs[0];
+            int i = 1;
+            while(i < strs.length){
+                while(strs[i].indexOf(pre) != 0)
+                    pre = pre.substring(0,pre.length()-1);
+                i++;
+            }
+            return pre;
+        }
+    }
+
+}
