3367d98cc5320518baebbda0d98f1bbf49be07b4
ray <277450201@qq.com>
2017-03-27 21:34:56 +0800

    Q2 Q3 finish

--- Q2AddTwoNumbers.java
+++ Q2AddTwoNumbers.java
@@ -1,30 +1,34 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by ray on 3/26/17.
+ * Created by ray on 1/3/17.
  */
-public class linkedlistcycleII {
-    public ListNode detectCycle(ListNode head) {
+public class AddTwoNumbers {
+    public class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode prev = new ListNode(0);
+            ListNode head = prev;
 
-        if(head==null) return null;
+            int carry = 0;
 
-        ListNode first = head;
-        ListNode second = head;
+            while (l1 != null || l2 != null || carry != 0) {
+                ListNode cur = new ListNode(0);
+                int sum = (l1== null ?0:l1.val) + (l2 == null ?0:l2.val) + carry;
+                cur.val = sum%10;
+                carry = sum/10;
+                prev.next =cur;
+                prev = cur;
 
-        while (second.next != null  && second.next.next != null ) {
-            first = first.next;
-            second = second.next.next;
 
-            if(first == second) {
-                while(head != first) {
-                    head = head.next;
-                    first = first.next;
-                }
-                return first;
+                l1 = (l1 == null) ? l1 : l1.next;
+                l2 = (l2 == null) ? l2 : l2.next;
             }
-        }
 
-        return null;
+            return head.next;
+        }
     }
 }
-
