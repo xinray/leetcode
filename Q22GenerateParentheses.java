842b669927f0d37921606e4e41d5649df20a1c35
ray <277450201@qq.com>
2017-04-03 12:38:13 +0800

    Q22 Q9 finish

--- Q22GenerateParentheses.java
+++ Q22GenerateParentheses.java
@@ -1,20 +1,32 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by ray on 3/27/17.
+ * Created by ray on 3/28/17.
  */
-public class Q24SwapNodesinPairs {
-    public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
+public class Q22GenerateParentheses {
+    public class Solution {
+        public List<String> generateParenthesis(int n) {
+            List<String> result = new ArrayList<>();
+            backtrack(result,"",0 ,0,n);
 
-        ListNode ll = head;
-        ListNode temp = head;
+            return  result;
+        }
+        public void backtrack(List<String> list, String str,int open  ,int end, int n){
 
-        ll = head.next;
-        temp = head.next.next;
-        head.next = swapPairs(temp);
-        ll.next =head;
+            if(str.length() == n*2) {
+                list.add(str);
+                return;
+            }
 
-        return ll;
+            if(open<n) {
+                backtrack(list,str+"(",open+1,end,n);
+            }
+            if(end <open){
+                backtrack(list, str + ")", open, end + 1, n);
+            }
+        }
     }
 }
