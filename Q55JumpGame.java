7223b56187ef04c33aa63dd321c441f950e85c7c
ray <277450201@qq.com>
2017-07-09 00:30:46 +0800

    no message

---
+++ Q55JumpGame.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * Created by ray on 7/8/17.
+ */
+public class Q55JumpGame {
+    public class Solution {
+        public boolean canJump(int[] nums) {
+            if(nums.length < 2) return true;
+
+            for(int i = nums.length-2; i>=0;i--) {
+                if(nums[i]  == 0) {
+                    int numsteep = 1;
+                    while(numsteep > nums[i]) {
+                        numsteep++;
+                        i--;
+                        if(i< 0) return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
