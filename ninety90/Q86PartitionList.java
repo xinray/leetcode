d1489f5270721146f75dd53ef7ef5325bc31ee45
ray <277450201@qq.com>
2017-07-11 09:48:46 +0800

    Q90

--- ninety90/Q86PartitionList.java
+++ ninety90/Q86PartitionList.java
@@ -5,6 +5,20 @@ package leetcode.ninety90;
  */
 public class Q86PartitionList {
     public ListNode partition(ListNode head, int x) {
-
+        ListNode dummy1 = new ListNode(0),dummy2 = new ListNode(0);
+        ListNode small = dummy1,big = dummy2;
+        while(head!=null) {
+            if(head.val >= x) {
+                big.next = head;
+                big = head;
+            } else {
+                small.next = head;
+                small = head;
+            }
+            head = head.next;
+        }
+        big.next = null;
+        small.next = dummy2.next;
+        return dummy1.next;
     }
 }
